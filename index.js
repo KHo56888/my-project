1cb7e8d380cdb6103f96388f77116acca9040741
KHo56888 <88735037+KHo56888@users.noreply.github.com>
2021-10-09 19:05:58 +0100

    Reordered

--- index.js
+++ index.js
@@ -109,4 +109,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-showCity("Paris");
+showCity("London");
